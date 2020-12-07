1e1f8d68881363e9468dc94074639e68ce3956db
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-12-07 13:55:49 +0100

    bubbleSort funktionen

--- sorting.js
+++ sorting.js
@@ -1,3 +1,20 @@
-import { randomArray } from './helpers.js'
+import { swap, randomArray } from './helpers.js'
 
 const a = randomArray(100, 10)
+
+console.log(a)
+
+function bubbleSort(a){
+    for (let j = 0; j < a.length; j++) {
+        for (let i = 0; i < a.length; i++) {
+            if(a[i] > a[i + 1]) {
+                swap (a, i, i + 1)
+            }
+        }
+    }
+    return a
+}
+
+console.log(bubbleSort(a))
+
+console.log(bubbleSort([9, 1, 6, 8, 4]))
