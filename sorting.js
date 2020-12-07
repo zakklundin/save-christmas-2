7dcb7224fb82a2b5efe2a23f29a72611a7724636
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-12-07 15:15:02 +0100

    Selection Sort

--- sorting.js
+++ sorting.js
@@ -18,3 +18,16 @@ function bubbleSort(a){
 console.log(bubbleSort(a))
 
 console.log(bubbleSort([9, 1, 6, 8, 4]))
+
+
+function findMinIndex(a, m, n) {
+    let Min = m
+    for (let i = m; i < n + 1; i++) { //n+1 eftersom n motsvarar en position i ett array
+        if(a[i] < a[Min]){
+            Min = i
+        }
+    }
+    return Min
+}
+const arr = [5, -6, 0, 3, -1]
+console.log(findMinIndex(arr, 1, 3)) //borde ge 1 eftersom arr[1] är minst
